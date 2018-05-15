0240a1dd2326b52f163b32685027905c4da26b0c
risk1996 <williamdariansutedjo@gmail.com>
2018-05-15 11:19:27 +0700

    Item Activity unit conversion

--- app/src/main/java/id/ac/umn/mobile/babel/ItemActivity.kt
+++ app/src/main/java/id/ac/umn/mobile/babel/ItemActivity.kt
@@ -1,28 +1,18 @@
 package id.ac.umn.mobile.babel
 
-import android.content.Context
 import android.content.Intent
-import android.media.Image
+import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.GridLayoutManager
+import android.support.v4.content.ContextCompat
 import android.text.Editable
+import android.text.InputFilter
+import android.text.InputType
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.widget.*
-import com.google.android.gms.common.util.Hex
-import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.FirebaseDatabase
-import org.w3c.dom.Text
-import java.security.MessageDigest
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashSet
-import kotlin.collections.LinkedHashSet
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
+import java.text.DecimalFormat
 
 class ItemActivity : AppCompatActivity() {
     //    HEAD
@@ -38,6 +28,7 @@ class ItemActivity : AppCompatActivity() {
         val unitMeasureS = findViewById<Spinner>(R.id.activity_item_spn_unit_measure)
         val unitNameS = findViewById<Spinner>(R.id.activity_item_spn_unit_name)
         val safetyStockET = findViewById<EditText>(R.id.activity_item_et_item_safety_stock)
+        val stockPerLocationTL = findViewById<TableLayout>(R.id.activity_item_tl_stock_per_location)
         val cancelB = findViewById<Button>(R.id.activity_item_btn_cancel)
         val okB = findViewById<Button>(R.id.activity_item_btn_ok)
 
@@ -60,25 +51,7 @@ class ItemActivity : AppCompatActivity() {
             }
             "NEW" -> {
                 titleTV.text = "NEW ITEM"
-//                unitMeasureS.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-//                    override fun onNothingSelected(parent: AdapterView<*>?) {}
-//                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-//                        val unitName = ArrayAdapter<String>(this@ItemActivity, android.R.layout.simple_list_item_1)
-//                        data.units.filter{ it.measure == parent!!.getItemAtPosition(position) }.forEach{unitName.add(it.unit_name)}
-//                        unitNameS.adapter = unitName
-//                    }
-//                }
-//                okB.setOnClickListener{
-//                    finish()
-//                    val db = FirebaseDatabase.getInstance().reference.child("items")
-//                    val items = mutableMapOf<String, Any>()
-//                    items["item_name"] = itemNameET.text.toString()
-//                    items["item_thumbnail"] = "icons8_circled_b_48" // TODO("update thumbnail biar gak literal")
-//                    items["safety_stock"] = safetyStockET.text.toString()
-//                    items["stocks"] = mutableListOf(999.toString()).toList()
-//                    items["unit_id"] = ((unitMeasureS.selectedItemPosition)*100 + 101 + unitNameS.selectedItemPosition).toString()
-//                    db.child((data.items.size + 1).toString()).setValue(items)
-//                }
+                okB.text = "Confirm Addition"
             }
             else -> { finish() }
         }
@@ -87,32 +60,66 @@ class ItemActivity : AppCompatActivity() {
             override fun onComplete() {
                 val availMeasure = units.distinctBy { it.measure }.map { it.measure }
                 unitMeasureS.adapter = ArrayAdapter<String>(this@ItemActivity, android.R.layout.simple_list_item_1, availMeasure)
+                var availUnits = units.filter { it.measure == availMeasure[unitMeasureS.selectedItemPosition] }
                 val item  = items.singleOrNull { it._id == itemID }
                 val unit = units.singleOrNull { it._id == item?.unit_id }
+                val rawStock = ArrayList<Double>()
+                val stockETs = ArrayList<TextView>()
                 if (act == "EDIT" || act == "VIEW") {
                     item!!; unit!!
                     itemThumbnailIB.setImageResource(R.drawable::class.java.getField(item.thumbnail.replace("_48","_96")).getInt(null))
                     itemNameET.setText(item.itemName)
                     unitMeasureS.setSelection(availMeasure.indexOf(unit.measure))
-                    safetyStockET.setText(item.safetyStock.toString())
+                    safetyStockET.setText((item.safetyStock / unit.value).toString())
+                    rawStock.add(item.safetyStock)
+                    stockETs.add(safetyStockET)
+                }
+                locations.forEach {
+                    val rowTR = TableRow(this@ItemActivity)
+                    val locationTV = TextView(this@ItemActivity)
+                    val stockOnLocationET = EditText(this@ItemActivity)
+                    locationTV.text = it.code
+                    rowTR.addView(locationTV, TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f))
+                    if (act == "EDIT" || act == "VIEW") stockOnLocationET.setText(item!!.stocks[locations.indexOf(it)].toString())
+                    else stockOnLocationET.setText(0f.toString())
+                    rawStock.add(stockOnLocationET.text.toString().toDouble())
+                    stockETs.add(stockOnLocationET)
+                    if (act == "VIEW") stockOnLocationET.isEnabled = false
+                    stockOnLocationET.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+                    rowTR.addView(stockOnLocationET, TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f))
+                    stockPerLocationTL.addView(rowTR)
+                }
+                stockETs.forEach {
+                    it.addTextChangedListener(object : TextWatcher{
+                        override fun afterTextChanged(p0: Editable?) {}
+                        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                            rawStock[stockETs.indexOf(it)] = (if(it.text.toString() != "") it.text.toString().toDouble() else .0) * availUnits[unitNameS.selectedItemPosition].value
+                        }
+                    })
                 }
                 unitMeasureS.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
                     override fun onNothingSelected(parent: AdapterView<*>?) {}
                     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                        val availUnits = units.filter { it.measure == availMeasure[unitMeasureS.selectedItemPosition] }
+                        availUnits = units.filter { it.measure == availMeasure[unitMeasureS.selectedItemPosition] }
                         unitNameS.adapter = ArrayAdapter<String>(this@ItemActivity, android.R.layout.simple_list_item_1, availUnits.map { it.unit_name })
                         if ((act == "VIEW" || act == "EDIT") && unit!!.measure == availMeasure[unitMeasureS.selectedItemPosition]) unitNameS.setSelection(availUnits.indexOf(unit))
                         else unitNameS.setSelection(0)
+                    }
+                }
+                unitNameS.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                    override fun onNothingSelected(parent: AdapterView<*>?) {}
+                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                        stockETs.forEach {it.text = DecimalFormat("0.###############").format(rawStock[stockETs.indexOf(it)] / availUnits[unitNameS.selectedItemPosition].value) }
                     }
                 }
                 okB.setOnClickListener{
                     Toast.makeText(this@ItemActivity, "HAI HAI", Toast.LENGTH_SHORT).show()
                     when (act) {
                         "VIEW" -> {
-                            item!!
                             val intent = Intent(this@ItemActivity, ItemActivity::class.java)
                             intent.putExtra("OPERATION", "EDIT")
-                            intent.putExtra("ITEM_ID", item._id)
+                            intent.putExtra("ITEM_ID", item!!._id)
                             startActivity(intent)
                         }
                         "EDIT" -> {
